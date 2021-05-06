59b6830626f966944241c8be9e0764451aa42fe3
Moutinho <alexandre.moutinho@live.fr>
2021-05-06 17:13:38 +0200

    :wrench: add server.js

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const app = require("./app");
+
+//Permet de retourner un port valide, quil soit obtenu sous la forme du nombre ou d'une chaine de caractères
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port); //On indique qu'elle port utiliser dans app.js
+
+//Permet de renvoyer les erreurs afin de les traiter
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app); //Construit le serveur avec le protocol http et en utilisant app.js
+
+server.on("error", errorHandler); //lance le gestionnaire d'erreurs
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+
+server.listen(port);
