a69dae785644f063fd821e203d6d0e365f3467fc
Moutinho <alexandre.moutinho@live.fr>
2021-05-07 15:24:49 +0200

    :wrench: add user.js models

---
+++ backend/models/user.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Sequelize, DataTypes, database } = require('./connexion');
+
+const User = database.define(
+  'User',
+  {
+    username: {
+      type: DataTypes.STRING,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      required: true,
+      unique: true,
+    },
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0,
+    },
+  },
+  {
+    Sequelize,
+    modelName: 'User',
+  }
+);
+
+module.exports = User;
