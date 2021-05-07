eb82b51b42c71d862288b8e9c9ca6cc8bb78deb1
Moutinho <alexandre.moutinho@live.fr>
2021-05-07 14:38:32 +0200

    :hammer: update user.js routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router(); //Permet de charger le middleware niveau router
 
 const userCtrl = require("../controllers/user"); //On appelle la logique métier de nos routes
-const authenticationLimiter = require("../middleware/authenticationLimiter"); //On appelle le middleware sauceLimiter
+
+//TODO const authenticationLimiter = require("../middleware/authenticationLimiter"); //On appelle le middleware authentificationLimiter
 
 router.post("/signup", userCtrl.signup); //Créer un nouvel utilisateur
-router.post("/login", authenticationLimiter, userCtrl.login); //Login d'un utilisateur existant
+router.post("/login", userCtrl.login); //Login d'un utilisateur existant TODO:authenticationLimiter
 
 module.exports = router;
