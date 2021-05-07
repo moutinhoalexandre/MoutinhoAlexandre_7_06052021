f48c24cc9ec7c92d7b4628bf597c42f9c664f3e4
Moutinho <alexandre.moutinho@live.fr>
2021-05-07 15:49:24 +0200

    :wrench: add config.js

---
+++ backend/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+  },
+};
