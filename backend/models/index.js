c3772ffc63250a675653ad6201599053fa814298
Moutinho <alexandre.moutinho@live.fr>
2021-05-10 14:29:57 +0200

    :hammer: update index.js models

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,13 @@
 const User = require("../models/user");
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 User.hasMany(Post, { onDelete: "CASCADE" });
+
 Post.belongsTo(User, { foreignKey: "userId", onDelete: "CASCADE" });
+Post.hasMany(Comment, { onDelete: "CASCADE" });
+
+Comment.belongsTo(Post, { foreignKey: "postId", onDelete: "CASCADE" });
+Comment.belongsTo(User, { foreignKey: "userId", onDelete: "CASCADE" });
 
 module.exports = { User, Post };
