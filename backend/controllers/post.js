d2467958c4184105bb6b40f8d3ac1781c6b8a3cc
Moutinho <alexandre.moutinho@live.fr>
2021-05-12 22:25:36 +0200

    :hammer: update getOnePost in post.js controllers

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -80,7 +80,7 @@ exports.deletePost = (req, res, next) => {
 
 //Afficher un post
 exports.getOnePost = (req, res, next) => {
-  Post.findOne({ id: req.params.id })
+    Post.findOne({ where: { id: req.params.id } })
   .then(post => res.status(200).json(post))
   .catch(error => res.status(404).json({ error }));
 };
