9d4f103114ba507f41311ac17702e2e78fc7203c
Moutinho <alexandre.moutinho@live.fr>
2021-06-10 11:40:44 +0200

    :hammer: update component log

--- frontend/src/component/log/Authentification.js
+++ frontend/src/component/log/Authentification.js
@@ -1,15 +1,9 @@
 import React from "react";
 import "./Authentification.css";
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebookF,
-  faTwitter,
-  faGoogle,
-  faLinkedinIn,
-} from "@fortawesome/free-brands-svg-icons";
-import { faLock, faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Logo from "../../assets/icon-left-font-monochrome-black.png";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 export default function Authentification() {
   const [signIn, setSignIn] = useState(false);
@@ -23,112 +17,8 @@ export default function Authentification() {
       <div className={signIn ? "container sign-up-mode" : "container"}>
         <div className="forms-container">
           <div className="signin-signup">
-            <form action="#" className="sign-in-form">
-              <h2 className="title">Sign in</h2>
-              <div className="input-field">
-                <i className="fas fa-user">
-                  <FontAwesomeIcon icon={faUser} />
-                </i>
-                <input type="text" placeholder="Username" className="input" />
-              </div>
-              <div className="input-field">
-                <i className="fas fa-lock">
-                  <FontAwesomeIcon icon={faLock} />
-                </i>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="input"
-                />
-              </div>
-              <input type="submit" value="Login" className="btn solid" />
-              <p className="social-text">
-                Coming Soon : sign in with social platforms
-              </p>
-              <div className="social-media">
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-facebook-f">
-                    <FontAwesomeIcon icon={faFacebookF} />
-                  </i>
-                </a>
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-twitter">
-                    <FontAwesomeIcon icon={faTwitter} />
-                  </i>
-                </a>
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-google">
-                    <FontAwesomeIcon icon={faGoogle} />
-                  </i>
-                </a>
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-linkedin-in">
-                    <FontAwesomeIcon icon={faLinkedinIn} />
-                  </i>
-                </a>
-              </div>
-            </form>
-            <form action="#" className="sign-up-form">
-              <h2 className="title">Sign up</h2>
-              <div className="input-field">
-                <i className="fas fa-user">
-                  <FontAwesomeIcon icon={faUser} />
-                </i>
-                <input type="text" placeholder="Username" className="input" />
-              </div>
-              <div className="input-field">
-                <i className="fas fa-envelope">
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </i>
-                <input type="email" placeholder="Email" className="input" />
-              </div>
-              <div className="input-field">
-                <i className="fas fa-lock">
-                  <FontAwesomeIcon icon={faLock} />
-                </i>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="input"
-                />
-              </div>
-              <div className="input-field">
-                <i className="fas fa-lock">
-                  <FontAwesomeIcon icon={faLock} />
-                </i>
-                <input
-                  type="password"
-                  placeholder="Confirm password"
-                  className="input"
-                />
-              </div>
-              <input type="submit" className="btn" value="Sign up" />
-              <p className="social-text">
-                Coming Soon : sign in with social platforms
-              </p>
-              <div className="social-media">
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-facebook-f">
-                    <FontAwesomeIcon icon={faFacebookF} />
-                  </i>
-                </a>
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-twitter">
-                    <FontAwesomeIcon icon={faTwitter} />
-                  </i>
-                </a>
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-google">
-                    <FontAwesomeIcon icon={faGoogle} />
-                  </i>
-                </a>
-                <a href="#top" className="social-icon">
-                  <i className="fab fa-linkedin-in">
-                    <FontAwesomeIcon icon={faLinkedinIn} />
-                  </i>
-                </a>
-              </div>
-            </form>
+            <SignIn />
+            <SignUp />
           </div>
         </div>
 
--- frontend/src/component/log/SignIn.js
+++ frontend/src/component/log/SignIn.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './SignIn.css';
+import React from "react";
+import "./SignIn.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookF,
@@ -10,49 +10,49 @@ import {
 import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function SignIn() {
-    return (
-      <>
-        <form action="#" className="sign-in-form">
-          <h2 className="title">Sign in</h2>
-          <div className="input-field">
-            <i className="fas fa-user">
-              <FontAwesomeIcon icon={faUser} />
+  return (
+    <>
+      <form action="#" className="sign-in-form">
+        <h2 className="title">Sign in</h2>
+        <div className="input-field">
+          <i className="fas fa-user">
+            <FontAwesomeIcon icon={faUser} />
+          </i>
+          <input type="text" placeholder="Username" className="input" />
+        </div>
+        <div className="input-field">
+          <i className="fas fa-lock">
+            <FontAwesomeIcon icon={faLock} />
+          </i>
+          <input type="password" placeholder="Password" className="input" />
+        </div>
+        <input type="submit" value="Login" className="btn solid" />
+        <p className="social-text">
+          Coming Soon : sign in with social platforms
+        </p>
+        <div className="social-media">
+          <a href="#top" className="social-icon">
+            <i className="fab fa-facebook-f">
+              <FontAwesomeIcon icon={faFacebookF} />
             </i>
-            <input type="text" placeholder="Username" className="input" />
-          </div>
-          <div className="input-field">
-            <i className="fas fa-lock">
-              <FontAwesomeIcon icon={faLock} />
+          </a>
+          <a href="#top" className="social-icon">
+            <i className="fab fa-twitter">
+              <FontAwesomeIcon icon={faTwitter} />
             </i>
-            <input type="password" placeholder="Password" className="input" />
-          </div>
-          <input type="submit" value="Login" className="btn solid" />
-          <p className="social-text">
-            Coming Soon : sign in with social platforms
-          </p>
-          <div className="social-media">
-            <a href="#top" className="social-icon">
-              <i className="fab fa-facebook-f">
-                <FontAwesomeIcon icon={faFacebookF} />
-              </i>
-            </a>
-            <a href="#top" className="social-icon">
-              <i className="fab fa-twitter">
-                <FontAwesomeIcon icon={faTwitter} />
-              </i>
-            </a>
-            <a href="#top" className="social-icon">
-              <i className="fab fa-google">
-                <FontAwesomeIcon icon={faGoogle} />
-              </i>
-            </a>
-            <a href="#top" className="social-icon">
-              <i className="fab fa-linkedin-in">
-                <FontAwesomeIcon icon={faLinkedinIn} />
-              </i>
-            </a>
-          </div>
-        </form>
-      </>
-    );
+          </a>
+          <a href="#top" className="social-icon">
+            <i className="fab fa-google">
+              <FontAwesomeIcon icon={faGoogle} />
+            </i>
+          </a>
+          <a href="#top" className="social-icon">
+            <i className="fab fa-linkedin-in">
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </i>
+          </a>
+        </div>
+      </form>
+    </>
+  );
 }
